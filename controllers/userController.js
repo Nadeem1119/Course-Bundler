1ebb9ce63834aec52cc7bdca659d247e70a9b206
Nadeem Ahmad <nadeemahmad1119990@gmail.com>
2024-04-24 12:06:26 +0530

    changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -165,7 +165,7 @@ const user= await User.findOne({email}).select("+password");
 
         await user.save();
 
-        const url = `http://localhost:3000/resetpassword/${resetToken}`
+        const url = `${process.env.FRONTEND_URL}/resetpassword/${resetToken}`
 
         const message=`Click on the link to reset your password. ${url} . If you have not requested then please ignore it `;
        
