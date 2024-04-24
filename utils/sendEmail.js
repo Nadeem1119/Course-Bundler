7a8e2b9e9a309c6627eacddf872d30f44a4f77f6
Nadeem Ahmad <nadeemahmad1119990@gmail.com>
2024-04-24 15:03:38 +0530

    changes

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -4,14 +4,6 @@ export const sendEmail = async(to,subject,text)=>{
 
   const transporter=createTransport(
     {
-      /*
-      host : "sandbox.smtp.mailtrap.io",
-      port : 2525,
-      auth:{
-        user:"cad9d9497df62f",
-        pass:"cb9e08b90ca382"
-      }
-      */
       host: process.env.SMTP_HOST,
       port: process.env.SMTP_PORT,
       auth: {
