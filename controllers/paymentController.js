e2cc493dc21f97c98016d574c63de9617a6c388e
Nadeem Ahmad <nadeemahmad1119990@gmail.com>
2024-05-07 21:53:49 +0530

    changes

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -6,11 +6,12 @@ import ErrorHandler from "../utils/ErrorHandler.js";
 
 
 
-export const buySubscription=catchAsyncError(async (req,res,next)=>{
+export const buySubscription=catchAsyncError(async (req,res,next) => {
 
- const user=await User.findById(req.user._id);
+ const user = await User.findById(req.user._id);
 
- if(user.role==="admin") return next(new ErrorHandler("Admin can't buy a Subscription",404))
+ if(user.role==="admin") 
+  return next(new ErrorHandler("Admin can't buy a Subscription",404))
 
  const plan_id = process.env.PLAN_ID || "plan_N0MJ62oeC7YQQG";
 
@@ -18,7 +19,7 @@ const subscription = await instance.subscriptions.create({
   plan_id,
   customer_notify: 1,
   total_count: 12,
- })
+ });
 
  user.subscription.id = subscription.id;
 
@@ -34,33 +35,35 @@ const subscription = await instance.subscriptions.create({
 
 export const paymentVerification = catchAsyncError(async (req,res,next)=>{
 
-  const {razorpay_signature, razorpay_subscription_id,razorpay_payment_id }=req.body;
+  const { razorpay_signature,razorpay_payment_id, razorpay_subscription_id } = req.body;
 
   const user = await User.findById(req.user._id);
 
   const subscription_id = user.subscription.id;
  
-  const generated_signature=crypto.createHmac("sha256",
-  process.env.RAZORPAY_API_SECRET).
-  update(razorpay_payment_id + "|" + subscription_id,"utf-8")
+  const generated_signature = crypto
+  .createHmac("sha256",process.env.RAZORPAY_API_SECRET)
+  .update(razorpay_payment_id + "|" + subscription_id,"utf-8")
   .digest("hex");
 
-  const isAuthentic= generated_signature===razorpay_signature;
+  const isAuthentic= generated_signature === razorpay_signature;
 
-  if(!isAuthentic) res.redirect(`${process.env.FRONTEND_URL}/paymentfail`)
+  if(!isAuthentic) 
+    return res.redirect(`${process.env.FRONTEND_URL}/paymentfail`);
 
   //Database comes here
   await Payment.create({
     razorpay_signature,
-    razorpay_subscription_id,
     razorpay_payment_id,
-  })
+    razorpay_subscription_id,
+  });
 
-  user.subscription.status="active";
+  user.subscription.status = "active";
 
   await user.save();
  
-  res.redirect(`${process.env.FRONTEND_URL}/paymentsucess?reference=${razorpay_payment_id}`
+  res.redirect(
+    `${process.env.FRONTEND_URL}/paymentsuccess?reference=${razorpay_payment_id}`
   );
  });
 
@@ -68,8 +71,8 @@ export const paymentVerification = catchAsyncError(async (req,res,next)=>{
   res.status(200).json({
     success:true,
     key:process.env.RAZORPAY_API_KEY,
-  })
- })
+  });
+ });
 
  export const cancelSubscription=catchAsyncError(async (req,res,next)=>{
 
@@ -87,7 +90,7 @@ export const paymentVerification = catchAsyncError(async (req,res,next)=>{
    
   const gap=Date.now()-payment.createdAt;
 
-  const refundTime=process.env.REFUND_DAYS *24*60*60*1000;
+  const refundTime=process.env.REFUND_DAYS*24*60*60*1000;
 
   if(refundTime>gap){
   await instance.payments.refund(payment.razorpay_payment_id);
