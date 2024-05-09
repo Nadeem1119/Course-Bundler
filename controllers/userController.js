17ffad683ad1ead9ab76c81c0f4c1c482dd89acd
Nadeem Ahmad <nadeemahmad1119990@gmail.com>
2024-05-09 08:51:46 +0530

    changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -313,9 +313,11 @@ const user= await User.findOne({email}).select("+password");
 
       const subscription = await User.find({ "subscription.status": "active" })
 
-      stats[0].users = await User.countDocuments();
-      stats[0].subscription = subscription.length;
-      stats[0].createdAt = new Date(Date.now());
+      if (stats && stats.length > 0) {
+        stats[0].users = await User.countDocuments();
+      }
+       stats[0].subscription = subscription.length;
+       stats[0].createdAt = new Date(Date.now());
 
       await stats[0].save();
       })
